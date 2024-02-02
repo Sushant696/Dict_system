06c58ea005f96ad12f4b2b4edd4b920aec272563
Nishan Bista <nishanbista525@gmail.com>
2024-02-02 22:17:15 +0545

    feature : checks the boxes are empty or not and button works

--- src/main/addWordWindow.py
+++ src/main/addWordWindow.py
@@ -5,6 +5,25 @@ def add_word_window():
     add_root.geometry("500x500")
     add_root.resizable(0,0)
 
+        #function that works when the add button is clicked and checks the boxes wheather they are empty or not
+    def submit():
+        if word.get()=="" and description.get("1.0", END) == "\n":
+            alert_message("Both Word and Description are Empty!")
+        elif word.get()=="":
+            alert_message("Word is Empty!")
+        elif description.get("1.0", END) == "\n":
+            alert_message("Description is Empty!")
+        else:
+            alert.config(text="")
+            word.delete(0,END)
+            description.delete("1.0",END)
+
+
+
+    #this is just a function that displays alert messages!
+    def alert_message(message):
+        alert.config(text=f"{message}")
+
     # labels
     word_label = Label(add_root,text="Word",font=16)
     word_label.place(x=80,y=20)
@@ -22,12 +41,19 @@ def add_word_window():
 
 
 # This is the button to submit / add word to the dictionary
-    add_button = Button(add_root,text="Add")
-    add_button.place(x=210,y=400,height=50,width=100)
+    add_button = Button(add_root,text="Add",command=submit)
+    add_button.place(x=210,y=380,height=50,width=100)
+
+
+
+# This is label for alert message output
+    alert =Label(text="",bg="red")
+    alert.place(x=200,y=460)
 
 
     add_root.mainloop()
 
+add_word_window()
 
 
 
